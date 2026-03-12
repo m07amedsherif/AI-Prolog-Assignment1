f6dd0e6ad28b399f87d1c50e8f2c80659ae4753e
mohamed sherif <162077612+m07amedsherif@users.noreply.github.com>
2026-03-12 13:36:03 +0200

    Refactor library solution with additional rules

--- library_solution.pl
+++ library_solution.pl
@@ -60,52 +60,91 @@ not_member(X, [H|T]) :-
     X \= H,
     not_member(X, T).
 
-%% Rule 4
-most_borrowed_from_list([B], B).
-most_borrowed_from_list([B1,B2|Rest], Max) :-
-    borrowers_count(B1, N1),
-    borrowers_count(B2, N2),
-    (N1 >= N2 ->
-        most_borrowed_from_list([B1|Rest], Max)
-    ;
-        most_borrowed_from_list([B2|Rest], Max)
-    ).
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%% Rule 4: List all ratings for a specific book as a list of tuples %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 ratings_of_book(Book, L) :-
-    findall((Student,Score), rating(Student, Book, Score), L).
+    collect_ratings(Book, [], L), !.
 
+collect_ratings(Book, Visited, [(Student, Score)|Rest]) :-
+    rating(Student, Book, Score),
+    not_member((Student, Score), Visited),
+    collect_ratings(Book, [(Student, Score)|Visited], Rest).
+
+collect_ratings(_, _, []).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%% Rule 5: Find the top reviewer in the library %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 top_reviewer(Student) :-
-    setof(Score-S, B^rating(S,B,Score), List),
-    last(List, _MaxScore-Student).
+    collect_all_ratings([], Ratings),
+    top_reviewer_from_list(Ratings, Student), !.
 
+collect_all_ratings(Visited, [(Student, Book, Score)|Rest]) :-
+    rating(Student, Book, Score),
+    not_member((Student, Book, Score), Visited),
+    collect_all_ratings([(Student, Book, Score)|Visited], Rest).
+
+collect_all_ratings(_, []).
+
+top_reviewer_from_list([(Student, _, _)], Student).
+
+top_reviewer_from_list([(S1, B1, Score1), (S2, B2, Score2)|Rest], Student) :-
+    Score1 >= Score2,
+    top_reviewer_from_list([(S1, B1, Score1)|Rest], Student).
+
+top_reviewer_from_list([(S1, B1, Score1), (S2, B2, Score2)|Rest], Student) :-
+    Score1 < Score2,
+    top_reviewer_from_list([(S2, B2, Score2)|Rest], Student).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%% Rule 6: Find the most common topic in books borrowed by a specific student %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 most_common_topic_for_student(Student, Topic) :-
     books_borrowed_by_student(Student, Books),
-    collect_topics(Books, Topics),
-    flatten(Topics, AllTopics),
-    most_frequent(AllTopics, Topic).
+    collect_borrowed_topics(Books, AllTopics),
+    collect_unique_topics(AllTopics, [], UniqueTopics),
+    most_common_from_unique(UniqueTopics, AllTopics, Topic), !.
 
-collect_topics([], []).
-collect_topics([B|Rest], [T|RT]) :-
-    topics(B, T),
-    collect_topics(Rest, RT).
+%% Collect all topics from all borrowed books into one big list
+collect_borrowed_topics([], []).
+collect_borrowed_topics([Book|RestBooks], AllTopics) :-
+    topics(Book, BookTopics),
+    collect_borrowed_topics(RestBooks, RestTopics),
+    merge_lists(BookTopics, RestTopics, AllTopics).
 
-most_frequent(List, Topic) :-
-    setof(X, member(X, List), Unique),
-    most_frequent_from_list(Unique, List, Topic).
+%% Merge two lists manually
+merge_lists([], L, L).
+merge_lists([H|T], L, [H|Rest]) :-
+    merge_lists(T, L, Rest).
 
-most_frequent_from_list([T], _, T).
-most_frequent_from_list([T1,T2|Rest], List, Max) :-
-    count(T1, List, C1),
-    count(T2, List, C2),
-    (C1 >= C2 ->
-        most_frequent_from_list([T1|Rest], List, Max)
-    ;
-        most_frequent_from_list([T2|Rest], List, Max)
-    ).
+%% Collect unique topics only once
+collect_unique_topics([], _, []).
+collect_unique_topics([Topic|Rest], Visited, [Topic|UniqueRest]) :-
+    not_member(Topic, Visited),
+    collect_unique_topics(Rest, [Topic|Visited], UniqueRest).
 
-count(_, [], 0).
-count(X, [X|T], N) :-
-    count(X, T, N1),
+collect_unique_topics([Topic|Rest], Visited, UniqueRest) :-
+    is_member(Topic, Visited),
+    collect_unique_topics(Rest, Visited, UniqueRest).
+
+%% Manual member check
+is_member(X, [X|_]).
+is_member(X, [_|T]) :-
+    is_member(X, T).
+
+%% Count how many times a topic appears
+count_topic(_, [], 0).
+count_topic(X, [X|T], N) :-
+    count_topic(X, T, N1),
     N is N1 + 1.
-count(X, [_|T], N) :-
-    count(X, T, N).
+count_topic(X, [H|T], N) :-
+    X \= H,
+    count_topic(X, T, N).
+
+%% Find the most frequent topic among unique topics
+most_common_from_unique([Topic], _, Topic).
+
+most_common_from_unique([T1, T2 | Rest], AllTopics, Topic) :-
+    count_topic(T1, AllTopics, C1),
+    count_topic(T2, AllTopics, C2),
+    C1 >= C2,
+    most_common_from_unique([T1 | Rest], AllTopics, Topic).
+
+most_common_from_unique([T1, T2 | Rest], AllTopics, Topic) :-
+    count_topic(T1, AllTopics, C1),
+    count_topic(T2, AllTopics, C2),
+    C1 < C2,
+    most_common_from_unique([T2 | Rest], AllTopics, Topic).
